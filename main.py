85959a64015b687b867d2701c239af40e39f7ec6
Gravity <tevoned@gmail.com>
2023-09-22 21:08:16 -0400

    highscore feature

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from food import Food
 from scoreboard import Scoreboard
 import time
 
+
+# screen setup
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
@@ -15,11 +17,13 @@ snake = Snake()
 food = Food()
 scoreboard = Scoreboard()
 
+# keys used for contolling snake movement 
 screen.listen()
 screen.onkey(snake.up, "Up")
 screen.onkey(snake.down, "Down")
 screen.onkey(snake.left, "Left")
 screen.onkey(snake.right, "Right")
+
 
 game_is_on = True
 while game_is_on:
@@ -36,14 +40,16 @@ while game_is_on:
 
 # collison with wall
     if snake.head.xcor() > 280 or  snake.head.xcor() < -280 or  snake.head.ycor() > 280 or snake.head.ycor() < -280:
-        game_is_on = False
-        scoreboard.game_over()
+        scoreboard.reset()
+     
     
 # Detect collision with tail
     for segment in snake.segments[1:]:
-        if snake.head.distance(segment) < 10:
-            game_is_on = False
-            scoreboard.game_over()
+        if segment == snake.head:
+            pass
+        elif snake.head.distance(segment) < 10:
+            scoreboard.reset()
+            
     
 
 
